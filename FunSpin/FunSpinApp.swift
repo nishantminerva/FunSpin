8ffee82e9f5e4d66e2f5bbcadfcd5bf29bc8e8ee
Nishant Minerva <nishantcafe@gmail.com>
2024-02-07 16:58:18 +0530

    Initial Commit

---
+++ FunSpin/FunSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunSpinApp.swift
+//  FunSpin
+//
+//  Created by Nishant Minerva on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FunSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
